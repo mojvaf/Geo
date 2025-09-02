7f622ce2b628847bfc084dbb402315193aaafd29
mojvaf <mojgan@zula-tribe.com>
2025-09-02 09:53:51 +0200

    clean

--- mainapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-class HomePage(TemplateView):
-    template_name = 'mainapp/home.html'
--- xproject/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from mainapp.views import HomePage
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', HomePage.as_view(), name='home')
-]
