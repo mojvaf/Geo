1df231b2373fee3afcad6a005b20e15dbe14f202
mojvaf <mojgan@zula-tribe.com>
2025-09-08 15:47:26 +0200

    added layout

--- frontend/_components/Logo.js
+++ frontend/_components/Logo.js
@@ -3,14 +3,8 @@ import Link from "next/link";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img
-        src="/bird.jpg"
-        height="70"
-        width="70"
-        className="w-[70px] h-[70px] rounded-full overflow-hidden"
-        alt="The Wild Oasis logo"
-      />
-      <span className="text-xl font-semibold text-primary-100">
+      <img src="/bird.jpg" height="90" width="90" alt="The Wild Oasis logo" />
+      <span className="text-xl font-semibold text-primary-100 ">
         The Wild Birds
       </span>
     </Link>
