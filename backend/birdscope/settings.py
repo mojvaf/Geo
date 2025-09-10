f2136fd6d4e9fc282535add355b803c46592bbf1
mojvaf <mojgan@zula-tribe.com>
2025-09-10 14:32:57 +0200

    added api

--- backend/birdscope/settings.py
+++ backend/birdscope/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",       
-    'species'
+     # Third-party apps
+    "rest_framework",  
+    "django_filters"  
+    # apps   
+    'species',
+    'api'
 ]
 
 MIDDLEWARE = [
--- backend/birdscope/urls.py
+++ backend/birdscope/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     ## API Endpoints
-    
+    path ('api/v1', include('api.urls') )
 ]
