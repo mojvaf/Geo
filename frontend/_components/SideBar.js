5d481168321c69af6463109cc42345689e7eb3e0
mojvaf <mojgan@zula-tribe.com>
2025-09-15 19:04:55 +0200

    align the map

--- frontend/_components/SideBar.js
+++ frontend/_components/SideBar.js
@@ -2,18 +2,25 @@ import Link from "next/link";
 
 export default function SideBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/adventure/city">Cities</Link>
-        </li>
-        <li>
-          <Link href="/adventure/country">Countries</Link>
-        </li>
-        <li>
-          <Link href="/adventure/form">Form</Link>
-        </li>
-      </ul>
-    </nav>
+    <aside className="flex flex-col items-center gap-4 w-full">
+      <h2 className="text-white text-xl mb-4">Adventure Menu</h2>
+      <nav className="flex flex-row gap-2">
+        <Link
+          href="/adventure/cities"
+          className="text-white hover:text-gray-300"
+        >
+          Cities
+        </Link>
+        <Link
+          href="/adventure/countries"
+          className="text-white hover:text-gray-300"
+        >
+          Countries
+        </Link>
+        <Link href="/adventure/form" className="text-white hover:text-gray-300">
+          Form
+        </Link>
+      </nav>
+    </aside>
   );
 }
--- frontend/app/adventure/layout.js
+++ frontend/app/adventure/layout.js
@@ -3,10 +3,14 @@ import SideBar from "@/_components/SideBar";
 
 export default function AdventureLayout({ children }) {
   return (
-    <div>
-      <SideBar />
-      {children}
-      <Map />
+    <div className="bg-primary-800 flex flex-col md:flex-row h-[calc(100vh-5.1rem)]">
+      <div className="w-full md:w-[40%] bg-primary-800 px-6 py-4 flex flex-col items-center">
+        <SideBar />
+        {children}
+      </div>
+      <div className="w-full md:w-[60%] h-full md:h-screen">
+        <Map />
+      </div>
     </div>
   );
 }
