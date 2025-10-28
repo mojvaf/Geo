635a0067f9198e1ab327b817ef9491924ef7632b
mojvaf <mojgan@zula-tribe.com>
2025-10-28 23:44:36 +0100

    added CORS

--- backend/birdscope/settings.py
+++ backend/birdscope/settings.py
@@ -33,14 +33,15 @@ INSTALLED_APPS = [
     'django_filters',
     'multiselectfield',
     'leaflet',
-
     'species',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -49,6 +50,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'birdscope.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 TEMPLATES = [
     {
@@ -123,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
