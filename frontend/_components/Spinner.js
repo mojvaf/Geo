165e1ee0a57ee87bb35e0637075fb7556d53ec18
mojvaf <mojgan@zula-tribe.com>
2025-11-04 10:45:58 +0100

    debug spinner and add error.js

--- frontend/_components/Spinner.js
+++ frontend/_components/Spinner.js
@@ -1,15 +1,5 @@
 function Spinner() {
-  return (
-    <div className="h-full flex item-center justify-center">
-      <div
-        className="spinner w-24 h-24 round-full animate-round ]"
-        style={{
-          WebkitMask:
-            "radial-gradient(farthest-side, transparent_calc(100% - 8px), theme('colors.primary.950'))",
-        }}
-      ></div>
-    </div>
-  );
+  return <div className="spinner"></div>;
 }
 
 export default Spinner;
