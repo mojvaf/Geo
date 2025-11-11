9991ab77a3666ffad337adf3768d8fa75b6f2838
mojvaf <mojgan@zula-tribe.com>
2025-11-11 17:14:09 +0100

    show the list

--- frontend/_components/CountryList.js
+++ frontend/_components/CountryList.js
@@ -1,10 +1,16 @@
-import CityItem from "./CityItem";
+"use client";
+
+import { useSelector } from "react-redux";
 
 function CountryList() {
+  const adventures = useSelector((state) => state.adventures.features);
+
   return (
     <div>
       <div className="text-6xl text-primary-50 mb-20 tracking-tight font-normal">
-        This is Country list
+        {adventures.map((feature) => (
+          <strong>{feature.properties.cityName}</strong>
+        ))}
       </div>
     </div>
   );
