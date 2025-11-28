2062e5654a86c2ce791b5f6e47fac4d27b557134
mojvaf <mojgan@zula-tribe.com>
2025-11-28 13:51:17 +0100

    fixed the icon

--- frontend/_components/Map.js
+++ frontend/_components/Map.js
@@ -1,29 +1,57 @@
 "use client";
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import L from "leaflet";
-
 import { fetchCities } from "../store/adventuresSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 
+// Fix Leaflet marker icons in Next.js
+const customIcon = new L.Icon({
+  iconUrl: "/icons/adventure.png",
+  iconRetinaUrl: "/icons/adventure.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 function Map() {
+  const [mapPosition, setMapPosition] = useState([43.656745, -79.488281]);
+
   const router = useRouter();
   const dispatch = useDispatch();
   const adventures = useSelector((state) => state.adventures.features);
-  // const searchParams = useSearchParams()
-  // const lat = searchParams.get('lat')
-  // const lng = searchParams.get('lng')
 
   useEffect(() => {
     dispatch(fetchCities());
   }, [dispatch]);
 
   return (
-    <>
-      <button onClick={() => router.push("/adventure/form")}>fff</button>
-    </>
+    <div
+      className="flex-1 h-full relative"
+      onClick={() => router.push("/adventure/form")}
+    >
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={mapPosition} icon={customIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
