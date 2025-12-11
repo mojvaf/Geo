0fcc766c0b14b36585f497fcd3b61ab6a4539853
mojvaf <mojgan@zula-tribe.com>
2025-12-11 16:18:11 +0100

    image bug fixed

--- frontend/_components/BirdItem.js
+++ frontend/_components/BirdItem.js
@@ -24,21 +24,28 @@ function BirdItem({ bird }) {
   console.log(bird);
 
   return (
-    <li className="w-full flex flex-col  gap-[1.4rem] sm:w-1/2 md:w-1/3">
-      <Link href="">
-        <div className="relative w-full h-64 rounded-xl overflow-hidden">
+    <li className="w-full sm:w-1/2 md:w-1/3 p-2">
+      <Link
+        href=""
+        className="flex flex-col h-[400px] bg-primary-800 rounded-xl overflow-hidden"
+      >
+        <div className="flex-shrink-0 relative w-full h-64 rounded-xl">
           <Image
             src={image}
             alt={name}
             fill
-            className="object-cover"
+            className="object-contain w-full h-full p-4 "
             quality={70}
           />
         </div>
-        <div>
-          Name: <span className="font-bold"> {name}</span>
+
+        {/* Content */}
+        <div className="flex-1 flex flex-col justify-between p-4 gap-2">
+          <div className="flex-1">
+            <div className="text-lg font-bold">{name}</div>
+            <div className="text-sm">{habitats}</div>
+          </div>
         </div>
-        <div>Habitats: {habitats}</div>
       </Link>
     </li>
   );
