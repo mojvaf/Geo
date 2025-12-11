d5c7ff0fc9312eff208c9aff718df9d5a3a76c71
mojvaf <mojgan@zula-tribe.com>
2025-12-11 17:45:36 +0100

    bird id route added

--- frontend/app/birds/[id]/page.js
+++ frontend/app/birds/[id]/page.js
@@ -1,9 +1,23 @@
-export default function Page({ params, searchParams }) {
-  const { id } = params;
+"use client";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBird } from "@/store/BirdsSlice";
+import Bird from "@/_components/Bird";
+import Loading from "@/_components/loading";
+import Message from "@/_components/Message";
 
-  return (
-    <div>
-      <h1>Bird </h1>
-    </div>
-  );
+export default function Page({ params }) {
+  const { id } = params;
+  const dispatch = useDispatch();
+
+  const bird = useSelector((state) => state.birds.current);
+  const status = useSelector((state) => state.birds.status);
+
+  useEffect(() => {
+    dispatch(fetchBird(id));
+  }, [dispatch, id]);
+
+  if (status === "idle" || !bird) return <Loading />;
+  if (status === "failed") return <Message message="Can not find the ID" />;
+  return <Bird bird={bird} />;
 }
